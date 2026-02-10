bb1ece74ac9665cc13a7cd88103af30ee3b28e41
Blanche Cérémonie <1476475770@qq.com>
2026-02-10 10:50:15 +0100

    dao and entity

--- src/main/java/com/example/demo/entity/student.java
+++ src/main/java/com/example/demo/entity/student.java
@@ -1,7 +1,23 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
+@Table(name="student")
 public class student {
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy = IDENTITY)
+    private long id;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="email")
+    private String email;
+
+    @Column(name="age")
+    private int age;
 }
--- src/main/java/com/example/demo/repository/StudentRepository.java
+++ src/main/java/com/example/demo/repository/StudentRepository.java
@@ -1,9 +1,10 @@
 package com.example.demo.repository;
 
+import com.example.demo.entity.student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StudentRepository extends JpaRepository {
+public interface StudentRepository extends JpaRepository<student,Long> {
 
 }
